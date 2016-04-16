297561fc92f89bb2e0dc6f3e92cee2ff8829c67d
Robert Butts <robert.o.butts@gmail.com>
2016-04-15 23:16:59 -0600

    Fix copydots.sh to copy init.el instead of .emacs

--- copydots.sh
+++ copydots.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ssh $1 'mkdir -p ~/.emacs.d/lisp'
-scp ~/.bash_profile ~/.profile ~/.emacs ~/.gitconfig ~/.tmux.conf ~/.docker-completion.sh ~/.git-completion.bash ~/.gitconfig $1:~
+scp ~/.bash_profile ~/.profile ~/.gitconfig ~/.tmux.conf ~/.docker-completion.sh ~/.git-completion.bash ~/.gitconfig $1:~
+scp ~/.emacs.d/init.el $1:~/.emacs.d
 scp ~/.emacs.d/lisp/go-mode.el ~/.emacs.d/lisp/go-mode-autoloads.el ~/.emacs.d/lisp/dockerfile-mode.el ~/.emacs.d/lisp/web-mode.el ~/.emacs.d/lisp/arc.el ~/.emacs.d/lisp/web-mode.el ~/.emacs.d/lisp/markdown-mode.el ~/.emacs.d/lisp/smex.el ~/.emacs.d/lisp/string-inflection.el $1:~/.emacs.d/lisp
