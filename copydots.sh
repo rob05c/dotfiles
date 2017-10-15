4cb2c44cde7afbade41e2342ff45f620909e5383
Robert Butts <robert.o.butts@gmail.com>
2017-10-15 16:36:57 -0600

    Update copydots.sh

--- copydots.sh
+++ copydots.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-ssh $1 'mkdir -p ~/.emacs.d/lisp'
-scp ~/.bash_profile ~/.profile ~/.gitconfig ~/.tmux.conf ~/.docker-completion.sh ~/.git-completion.bash ~/.gitconfig $1:~
-scp ~/.emacs.d/init.el $1:~/.emacs.d
-scp ~/.emacs.d/lisp/go-mode.el ~/.emacs.d/lisp/go-mode-autoloads.el ~/.emacs.d/lisp/dockerfile-mode.el ~/.emacs.d/lisp/web-mode.el ~/.emacs.d/lisp/arc.el ~/.emacs.d/lisp/web-mode.el ~/.emacs.d/lisp/markdown-mode.el ~/.emacs.d/lisp/smex.el ~/.emacs.d/lisp/string-inflection.el $1:~/.emacs.d/lisp
+# IPv6 addresses must NOT be enclosed in brackets
+ssh -4 $* 'mkdir -p ~/.emacs.d/lisp'
+scp -4 ~/.bash_profile ~/.profile ~/.emacs ~/.gitconfig ~/.tmux.conf ~/.docker-completion.sh \[$*\]:~
+scp -4 ~/.emacs.d/lisp/go-mode.el ~/.emacs.d/lisp/go-mode-autoloads.el ~/.emacs.d/lisp/dockerfile-mode.el ~/.emacs.d/lisp/web-mode.el ~/.emacs.d/lisp/arc.el ~/.emacs.d/lisp/web-mode.el ~/.emacs.d/lisp/markdown-mode.el ~/.emacs.d/lisp/smex.el ~/.emacs.d/lisp/string-inflection.el \[$*\]:~/.emacs.d/lisp
+scp -4 -r ~/.emacs.d/lisp/neotree \[$*\]:~/.emacs.d/lisp/neotree
+scp -4 ~/.emacs.d/init.el \[$*\]:~/.emacs.d
